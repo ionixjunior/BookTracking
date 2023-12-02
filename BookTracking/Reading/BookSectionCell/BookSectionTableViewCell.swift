e03ae21cc1e9db141e83db0c3808fa62bdadd235
Ione Souza Junior <junior@ionixjunior.com.br>
2023-12-02 18:02:14 -0300

    Create book section cell

---
+++ BookTracking/Reading/BookSectionCell/BookSectionTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  BookSectionTableViewCell.swift
+//  BookTracking
+//
+//  Created by Ione Souza Junior on 02/12/23.
+//
+
+import UIKit
+
+class BookSectionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
