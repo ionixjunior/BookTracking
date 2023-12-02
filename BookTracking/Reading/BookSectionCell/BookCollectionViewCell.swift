5ec64ca84c00f26641b7924b8dd783e47b85a41c
Ione Souza Junior <junior@ionixjunior.com.br>
2023-12-02 20:23:55 -0300

    Add book collection view cell

---
+++ BookTracking/Reading/BookSectionCell/BookCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  BookCollectionViewCell.swift
+//  BookTracking
+//
+//  Created by Ione Souza Junior on 02/12/23.
+//
+
+import UIKit
+
+class BookCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier: String = String(describing: BookCollectionViewCell.self)
+    
+}
