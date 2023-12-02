b4bcfcbd028e46e721c6528369892a105ea5980a
Ione Souza Junior <junior@ionixjunior.com.br>
2023-12-02 20:35:06 -0300

    Add image view

--- BookTracking/Reading/BookSectionCell/BookScreen.swift
+++ BookTracking/Reading/BookSectionCell/BookScreen.swift
@@ -8,11 +8,34 @@
 import UIKit
 
 class BookScreen: UIView {
+    var bookImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configScreen(view: UIView) {
+        addViews(view)
+        configConstraints(view)
+    }
+    
+    private func addViews(_ view: UIView) {
+        view.addSubview(bookImageView)
+    }
+    
+    private func configConstraints(_ view: UIView) {
+        NSLayoutConstraint.activate([
+            bookImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            bookImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
     }
 }
