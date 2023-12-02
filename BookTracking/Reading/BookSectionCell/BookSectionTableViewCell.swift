315c3ba82adec97c15f58d126c94353916fbeb49
Ione Souza Junior <junior@ionixjunior.com.br>
2023-12-02 18:44:18 -0300

    Add init for cell

--- BookTracking/Reading/BookSectionCell/BookSectionTableViewCell.swift
+++ BookTracking/Reading/BookSectionCell/BookSectionTableViewCell.swift
@@ -10,7 +10,15 @@ import UIKit
 class BookSectionTableViewCell: UITableViewCell {
     
     static let identifier: String = String(describing: BookSectionTableViewCell.self)
-
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
