8d08eee1e28332eb55e4d4a39336ca71ee7ffd17
Ione Souza Junior <junior@ionixjunior.com.br>
2023-12-02 18:43:26 -0300

    Remove nib override

--- BookTracking/Reading/BookSectionCell/BookSectionTableViewCell.swift
+++ BookTracking/Reading/BookSectionCell/BookSectionTableViewCell.swift
@@ -11,10 +11,6 @@ class BookSectionTableViewCell: UITableViewCell {
     
     static let identifier: String = String(describing: BookSectionTableViewCell.self)
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
-
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
