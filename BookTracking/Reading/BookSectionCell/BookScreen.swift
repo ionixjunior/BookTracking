3b03c5d307b5538d2e37c4834c472f5c98c25b47
Ione Souza Junior <junior@ionixjunior.com.br>
2023-12-02 20:30:14 -0300

    Add BookScreen

---
+++ BookTracking/Reading/BookSectionCell/BookScreen.swift
@@ -0,0 +1,18 @@
+//
+//  BookScreen.swift
+//  BookTracking
+//
+//  Created by Ione Souza Junior on 02/12/23.
+//
+
+import UIKit
+
+class BookScreen: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
