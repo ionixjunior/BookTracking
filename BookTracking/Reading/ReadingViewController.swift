255ad52ce07303c3a13b25ce8be1a2117e655c42
Ione Souza Junior <junior@ionixjunior.com.br>
2023-12-02 18:11:34 -0300

    Register cell for reuse

--- BookTracking/Reading/ReadingViewController.swift
+++ BookTracking/Reading/ReadingViewController.swift
@@ -39,6 +39,7 @@ class ReadingViewController: UIViewController {
     private func configTableView() {
         readingScreen?.tableView.delegate = self
         readingScreen?.tableView.dataSource = self
+        readingScreen?.tableView.register(BookSectionTableViewCell.nib, forCellReuseIdentifier: BookSectionTableViewCell.identifier)
     }
 
 }
