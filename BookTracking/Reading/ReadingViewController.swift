5c514fe8077374100cb05bb0342c45a80262c86c
Ione Souza Junior <junior@ionixjunior.com.br>
2023-12-02 17:50:36 -0300

    Add sample data

--- BookTracking/Reading/ReadingViewController.swift
+++ BookTracking/Reading/ReadingViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 class ReadingViewController: UIViewController {
 
     var readingScreen: ReadingScreen?
+    var bookSections: [BookSection] = [
+        BookSection(name: "", books: [
+            Book(name: "The Effective Executive", author: "Peter F. Drucker", image: "the_effective_executive"),
+            Book(name: "Grokking Algorithms", author: "Aditya Y. Bhargava", image: "grokking_algorithms"),
+            Book(name: "Design Patterns", author: "Erich Gamma, Richard Helm, and others", image: "design_patterns"),
+        ]),
+        BookSection(name: "Next Up", books: [
+            Book(name: "Testing Swift", author: "Paul Hudson", image: "testing_swift"),
+            Book(name: "Hacking with watchOS", author: "Paul Hudson", image: "hacking_with_watchos"),
+            Book(name: "Learning Domain-Driven Design", author: "Vlad Khononov", image: "learning_domain_driven_design"),
+            Book(name: "Clean Craftsmanship", author: "Robert C. Martin", image: "clean_craftsmanship"),
+            Book(name: "Code That Fits in Your Head", author: "Mark Seemann", image: "code_that_fits_in_your_head"),
+            Book(name: "Difficult Conversations", author: "Bruce Patton, Douglas Stone, and others", image: "difficult_conversations"),
+        ])
+    ]
     
     override func loadView() {
         readingScreen = ReadingScreen()
