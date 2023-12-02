ba2ca8c339ce67ad1814ce1d33b9021384994786
Ione Souza Junior <junior@ionixjunior.com.br>
2023-12-02 17:29:51 -0300

    Add table view

--- BookTracking/Reading/ReadingScreen.swift
+++ BookTracking/Reading/ReadingScreen.swift
@@ -8,13 +8,34 @@
 import UIKit
 
 class ReadingScreen: UIView {
+    
+    var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        addViews()
+        configConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addViews() {
+        addSubview(tableView)
+    }
+    
+    private func configConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 0),
+            tableView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: 0),
+        ])
     }
     
 }
