86b9d092fb13e8d4b92c969c24ec7c95fba7cbe0
Ione Souza Junior <junior@ionixjunior.com.br>
2023-12-02 22:36:47 -0300

    Configure background color

--- BookTracking/Reading/ReadingScreen.swift
+++ BookTracking/Reading/ReadingScreen.swift
@@ -18,6 +18,7 @@ class ReadingScreen: UIView {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        backgroundColor = .white
         addViews()
         configConstraints()
     }
