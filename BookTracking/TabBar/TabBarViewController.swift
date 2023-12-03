697989ec1949393ddb264e6e095dd8ddda8d7fa8
Ione Souza Junior <junior@ionixjunior.com.br>
2023-12-02 23:59:24 -0300

    Add more tab items

--- BookTracking/TabBar/TabBarViewController.swift
+++ BookTracking/TabBar/TabBarViewController.swift
@@ -15,14 +15,44 @@ class TabBarViewController: UITabBarController {
         return nav
     }()
     
+    private var books: UINavigationController = {
+        let vc = UIViewController()
+        vc.view.backgroundColor = .white
+        let nav = UINavigationController(rootViewController: vc)
+        return nav
+    }()
+    
+    private var bookmark: UINavigationController = {
+        let vc = UIViewController()
+        vc.view.backgroundColor = .white
+        let nav = UINavigationController(rootViewController: vc)
+        return nav
+    }()
+    
+    private var profile: UINavigationController = {
+        let vc = UIViewController()
+        vc.view.backgroundColor = .white
+        let nav = UINavigationController(rootViewController: vc)
+        return nav
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewControllers = [reading]
+        viewControllers = [reading, books, bookmark, profile]
         
         guard let items = tabBar.items else { return }
         
         items[0].image = UIImage(systemName: "book.fill")
         items[0].setImageOnly()
+        
+        items[1].image = UIImage(systemName: "books.vertical.fill")
+        items[1].setImageOnly()
+        
+        items[2].image = UIImage(systemName: "bookmark.fill")
+        items[2].setImageOnly()
+        
+        items[3].image = UIImage(systemName: "person.fill")
+        items[3].setImageOnly()
     }
     
     
