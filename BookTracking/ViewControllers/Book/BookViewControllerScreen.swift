e2109241c28559e515733123cc5e5e87801ecf77
Ione Souza Junior <junior@ionixjunior.com.br>
2023-12-06 23:48:55 -0300

    Add table view into book view controller

--- BookTracking/ViewControllers/Book/BookViewControllerScreen.swift
+++ BookTracking/ViewControllers/Book/BookViewControllerScreen.swift
@@ -8,14 +8,34 @@
 import UIKit
 
 class BookViewControllerScreen: UIView {
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .white
+        addViews()
+        configConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func addViews() {
+        addSubview(tableView)
+    }
+    
+    private func configConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+        ])
+    }
 }
