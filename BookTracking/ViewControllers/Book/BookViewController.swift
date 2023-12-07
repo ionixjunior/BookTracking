4d5225dfea4651e09a227a3c8795dd3e2fdbd616
Ione Souza Junior <junior@ionixjunior.com.br>
2023-12-06 22:59:01 -0300

    Add view controller initializer

--- BookTracking/ViewControllers/Book/BookViewController.swift
+++ BookTracking/ViewControllers/Book/BookViewController.swift
@@ -11,6 +11,17 @@ class BookViewController: UIViewController {
 
     private var screen: BookViewControllerScreen?
     
+    private var book: Book
+    
+    init(book: Book) {
+        self.book = book
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func loadView() {
         screen = BookViewControllerScreen()
         view = screen
