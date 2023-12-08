1b581fbee7f41b8f479d56e9c09d9621ff718144
Ione Souza Junior <junior@ionixjunior.com.br>
2023-12-08 19:34:19 -0300

    Add status property

--- BookTracking/Models/Book.swift
+++ BookTracking/Models/Book.swift
@@ -5,8 +5,15 @@
 //  Created by Ione Souza Junior on 02/12/23.
 //
 
+enum BookStatus {
+    case wantToRead
+    case inProgress
+    case read
+}
+
 struct Book {
     var name: String
     var author: String
     var image: String
+    var status: BookStatus
 }
