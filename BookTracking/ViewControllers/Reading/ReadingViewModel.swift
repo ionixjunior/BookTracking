9fd13aef11cf782576676d702922537fd747e413
Ione Souza Junior <junior@ionixjunior.com.br>
2023-12-08 19:35:34 -0300

    Update view model data

--- BookTracking/ViewControllers/Reading/ReadingViewModel.swift
+++ BookTracking/ViewControllers/Reading/ReadingViewModel.swift
@@ -10,17 +10,17 @@ import UIKit
 class ReadingViewModel {
     private var bookSections: [BookSection] = [
         BookSection(name: "", books: [
-            Book(name: "The Effective Executive", author: "Peter F. Drucker", image: "the_effective_executive"),
-            Book(name: "Grokking Algorithms", author: "Aditya Y. Bhargava", image: "grokking_algorithms"),
-            Book(name: "Design Patterns", author: "Erich Gamma, Richard Helm, and others", image: "design_patterns"),
+            Book(name: "The Effective Executive", author: "Peter F. Drucker", image: "the_effective_executive", status: .inProgress),
+            Book(name: "Grokking Algorithms", author: "Aditya Y. Bhargava", image: "grokking_algorithms", status: .inProgress),
+            Book(name: "Design Patterns", author: "Erich Gamma, Richard Helm, and others", image: "design_patterns", status: .inProgress),
         ]),
         BookSection(name: "Next Up", books: [
-            Book(name: "Testing Swift", author: "Paul Hudson", image: "testing_swift"),
-            Book(name: "Hacking with watchOS", author: "Paul Hudson", image: "hacking_with_watchos"),
-            Book(name: "Learning Domain-Driven Design", author: "Vlad Khononov", image: "learning_domain_driven_design"),
-            Book(name: "Clean Craftsmanship", author: "Robert C. Martin", image: "clean_craftsmanship"),
-            Book(name: "Code That Fits in Your Head", author: "Mark Seemann", image: "code_that_fits_in_your_head"),
-            Book(name: "Difficult Conversations", author: "Bruce Patton, Douglas Stone, and others", image: "difficult_conversations"),
+            Book(name: "Testing Swift", author: "Paul Hudson", image: "testing_swift", status: .wantToRead),
+            Book(name: "Hacking with watchOS", author: "Paul Hudson", image: "hacking_with_watchos", status: .wantToRead),
+            Book(name: "Learning Domain-Driven Design", author: "Vlad Khononov", image: "learning_domain_driven_design", status: .wantToRead),
+            Book(name: "Clean Craftsmanship", author: "Robert C. Martin", image: "clean_craftsmanship", status: .wantToRead),
+            Book(name: "Code That Fits in Your Head", author: "Mark Seemann", image: "code_that_fits_in_your_head", status: .wantToRead),
+            Book(name: "Difficult Conversations", author: "Bruce Patton, Douglas Stone, and others", image: "difficult_conversations", status: .wantToRead),
         ])
     ]
     
