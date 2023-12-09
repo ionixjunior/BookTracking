728c87c739c5912ef621c033c81990f8dd0753fb
Ione Souza Junior <junior@ionixjunior.com.br>
2023-12-09 20:08:37 -0300

    Add collection view inside table view cell

--- BookTracking/ViewControllers/Book/Cells/Detail/DetailTableViewCellScreen.swift
+++ BookTracking/ViewControllers/Book/Cells/Detail/DetailTableViewCellScreen.swift
@@ -9,6 +9,20 @@ import UIKit
 
 class DetailTableViewCellScreen: UIView {
     
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: 80, height: 160)
+        layout.minimumLineSpacing = 20
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.showsHorizontalScrollIndicator = false
+        
+        return cv
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
@@ -19,10 +33,12 @@ class DetailTableViewCellScreen: UIView {
     
     func configScreen(superView: UIView) {
         superView.addSubview(self)
+        addViews(superView)
+        configConstraints(superView)
     }
     
     private func addViews(_ superView: UIView) {
-        
+        addSubview(collectionView)
     }
     
     private func configConstraints(_ superView: UIView) {
@@ -31,6 +47,11 @@ class DetailTableViewCellScreen: UIView {
             bottomAnchor.constraint(equalTo: superView.bottomAnchor, constant: -20),
             leadingAnchor.constraint(equalTo: superView.leadingAnchor, constant: 20),
             trailingAnchor.constraint(equalTo: superView.trailingAnchor, constant: -20),
+            
+            collectionView.topAnchor.constraint(equalTo: topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor),
         ])
     }
     
