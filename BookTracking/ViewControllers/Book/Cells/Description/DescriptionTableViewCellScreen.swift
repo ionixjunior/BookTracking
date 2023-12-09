e73c4afd54381025a249c50a3d88e9dd191234aa
Ione Souza Junior <junior@ionixjunior.com.br>
2023-12-09 12:08:58 -0300

    Add description label

--- BookTracking/ViewControllers/Book/Cells/Description/DescriptionTableViewCellScreen.swift
+++ BookTracking/ViewControllers/Book/Cells/Description/DescriptionTableViewCellScreen.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class DescriptionTableViewCellScreen: UIView {
     
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: UILabel.appearance().font.fontName, size: 24)
+        label.textColor = .black
+        return label
+    }()
+    
     func configScreen(superView: UIView) {
         translatesAutoresizingMaskIntoConstraints = false
         addViews(superView)
@@ -16,11 +24,20 @@ class DescriptionTableViewCellScreen: UIView {
     }
     
     private func addViews(_ superView: UIView) {
-        
+        superView.addSubview(descriptionLabel)
     }
     
     private func configConstraints(_ superView: UIView) {
-        
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: superView.topAnchor),
+            bottomAnchor.constraint(equalTo: superView.bottomAnchor),
+            leadingAnchor.constraint(equalTo: superView.leadingAnchor, constant: 20),
+            trailingAnchor.constraint(equalTo: superView.trailingAnchor, constant: -20),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: superView.topAnchor, constant: 20),
+            descriptionLabel.leadingAnchor.constraint(equalTo: superView.leadingAnchor),
+            descriptionLabel.trailingAnchor.constraint(equalTo: superView.trailingAnchor),
+        ])
     }
 
 }
