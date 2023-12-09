fad3ab045a66bc9cf068e37dd8bf279f9a0a7bf4
Ione Souza Junior <junior@ionixjunior.com.br>
2023-12-08 21:56:30 -0300

    Create a component for book cover element

---
+++ BookTracking/Components/BookCoverComponent.swift
@@ -0,0 +1,24 @@
+//
+//  BookCoverComponent.swift
+//  BookTracking
+//
+//  Created by Ione Souza Junior on 08/12/23.
+//
+
+import UIKit
+
+class BookCoverComponent: UIImageView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        translatesAutoresizingMaskIntoConstraints = false
+        contentMode = .scaleAspectFill
+        clipsToBounds = true
+        layer.cornerRadius = 8
+        layer.borderColor = UIColor.systemGray5.cgColor
+        layer.borderWidth = 1
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
