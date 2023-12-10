d2c36d9dd5d6a64bf337379ea8485a753b773d31
Ione Souza Junior <junior@ionixjunior.com.br>
2023-12-10 10:51:32 -0300

    Add elements to the highlight label

--- BookTracking/ViewControllers/Book/Cells/Highlight/HighlightTableViewCellScreen.swift
+++ BookTracking/ViewControllers/Book/Cells/Highlight/HighlightTableViewCellScreen.swift
@@ -6,9 +6,22 @@
 //
 
 import UIKit
+import SnapKit
 
 class HighlightTableViewCellScreen: UIView {
 
+    private lazy var pageLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        return label
+    }()
+    
+    private lazy var quoteLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 24)
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
@@ -19,6 +32,36 @@ class HighlightTableViewCellScreen: UIView {
     
     func configScreen(superView: UIView) {
         superView.addSubview(self)
+        addViews()
+        configConstraints()
+    }
+    
+    private func addViews() {
+        addSubview(pageLabel)
+        addSubview(quoteLabel)
+    }
+    
+    private func configConstraints() {
+        snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+        
+        pageLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(4)
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.bottom.equalToSuperview().inset(4)
+        }
+        
+        quoteLabel.snp.makeConstraints { make in
+            make.top.equalTo(pageLabel.snp.bottom).offset(4)
+            make.leading.equalTo(pageLabel.snp.leading)
+            make.trailing.equalTo(pageLabel.snp.trailing)
+            make.bottom.equalToSuperview()
+        }
     }
     
 }
